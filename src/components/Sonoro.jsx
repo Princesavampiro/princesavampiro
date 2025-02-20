6ef826be28244420b9d39172c580e9a9c235ea71
astrosuka <yegresvoselok@gmail.com>
2025-02-20 14:42:22 -0300

    Fetch and display localized titles and descriptions in Sonoro component

--- src/components/Sonoro.jsx
+++ src/components/Sonoro.jsx
@@ -1,7 +1,26 @@
+import { useSections } from "../hooks/useData";
+import { PortableText } from "@portabletext/react";
+import useLanguage from "../hooks/useLanguage";
+
 export default function Sonoro() {
+  const { data, isLoading, error } = useSections();
+  const { language } = useLanguage();
+
+  if (isLoading) return <div>Cargando...</div>;
+  if (error) return <div>Hubo un error :( </div>;
+
+  const sonoroData = data.filter((i) => i._type === "sonoro")[0];
+
   return (
     <section>
-      <h1>Sonoro</h1>
+      <h1 className="text-xl">
+        {sonoroData.titulo[language] || sonoroData.titulo.es}
+      </h1>
+      {sonoroData.descripcion && (
+        <PortableText
+          value={sonoroData.descripcion[language] || sonoroData.descripcion.es}
+        />
+      )}
     </section>
   );
 }
