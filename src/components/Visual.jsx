88e2d72a5825d276fbf5abd91c928e2d5378685e
astrosuka <yegresvoselok@gmail.com>
2025-02-20 16:54:16 -0300

    Refactor Visual component to use SectionTitle for the title and display localized content

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,7 +1,27 @@
+import { useSections } from "../hooks/useData";
+import { PortableText } from "@portabletext/react";
+import useLanguage from "../hooks/useLanguage";
+import SectionTitle from "./SectionTitle";
+
 export default function Visual() {
+  const { data, isLoading, error } = useSections();
+  const { language } = useLanguage();
+
+  if (isLoading) return <div>Cargando...</div>;
+  if (error) return <div>Hubo un error :( </div>;
+
+  const visualData = data.filter((i) => i._type === "visual")[0];
+
   return (
     <section>
-      <h1>Visual</h1>
+      <SectionTitle
+        title={visualData.titulo[language] || visualData.titulo.es}
+      />
+      {visualData.descripcion && (
+        <PortableText
+          value={visualData.descripcion[language] || visualData.descripcion.es}
+        />
+      )}
     </section>
   );
 }
