202d41adb8338f7be8fdf9036116c59fc77afa70
astrosuka <yegresvoselok@gmail.com>
2025-02-20 16:53:58 -0300

    Refactor QuienSoy component to use SectionTitle and display localized bio content

--- src/components/QuienSoy.jsx
+++ src/components/QuienSoy.jsx
@@ -1,7 +1,27 @@
+import { useSections } from "../hooks/useData";
+import { PortableText } from "@portabletext/react";
+import useLanguage from "../hooks/useLanguage";
+import SectionTitle from "./SectionTitle";
+
 export default function QuienSoy() {
+  const { data, isLoading, error } = useSections();
+  const { language } = useLanguage();
+
+  if (isLoading) return <div>Cargando...</div>;
+  if (error) return <div>Hubo un error :( </div>;
+
+  const quienSoyData = data.filter((i) => i._type === "quienSoy")[0];
+
   return (
     <section>
-      <h1>Quien soy?</h1>
+      <SectionTitle
+        title={quienSoyData.titulo[language] || quienSoyData.titulo.es}
+      />
+      {quienSoyData.bio && (
+        <PortableText
+          value={quienSoyData.bio[language] || quienSoyData.bio.es}
+        />
+      )}
     </section>
   );
 }
