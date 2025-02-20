7387646db03850bee3a01a54b875e3196d2b203a
astrosuka <yegresvoselok@gmail.com>
2025-02-20 18:30:21 -0300

    Add PortableTextComponents for rendering custom text and media elements

---
+++ src/components/PortableText/PortableTextComponents.jsx
@@ -0,0 +1,32 @@
+import parse from "html-react-parser";
+
+export const components = {
+  types: {
+    embed: (props) => (
+      <div className="mx-auto my-4">{parse(props.value.html)}</div>
+    ),
+    image: (props) => (
+      <img className="mx-auto my-16 w-full max-w-4xl" src={props.value.url} />
+    ),
+  },
+  block: {
+    normal: ({ children }) => (
+      <p className="mx-auto w-full max-w-2xl">{children}</p>
+    ),
+    h1: ({ children }) => (
+      <h1 className="mx-auto w-full max-w-2xl text-xl sm:text-2xl">
+        {children}
+      </h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="font-display text-background-inverted mx-auto w-full max-w-2xl text-xl">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="font-display text-background-inverted mx-auto w-full max-w-2xl uppercase">
+        {children}
+      </h3>
+    ),
+  },
+};
