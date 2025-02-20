e81b1cf007162a19e0ed1644bdb7ebe833005422
astrosuka <yegresvoselok@gmail.com>
2025-02-20 16:54:25 -0300

    Refactor Visceral component to use SectionTitle and display localized content with links

--- src/components/Visceral.jsx
+++ src/components/Visceral.jsx
@@ -1,7 +1,38 @@
+import { useSections } from "../hooks/useData";
+import { PortableText } from "@portabletext/react";
+import useLanguage from "../hooks/useLanguage";
+import SectionTitle from "./SectionTitle";
+
 export default function Visceral() {
+  const { data, isLoading, error } = useSections();
+  const { language } = useLanguage();
+
+  if (isLoading) return <div>Cargando...</div>;
+  if (error) return <div>Hubo un error :( </div>;
+
+  const visceralData = data.filter((i) => i._type === "visceral")[0];
+
   return (
     <section>
-      <h1>Visceral</h1>
+      <SectionTitle title={visceralData.titulo} />
+      {visceralData.descripcion && (
+        <PortableText
+          value={
+            visceralData.descripcion[language] || visceralData.descripcion.es
+          }
+        />
+      )}
+      {visceralData.links && (
+        <ul>
+          {visceralData.links.map((link) => (
+            <li key={link._id}>
+              <a href={link.url} target="_blank">
+                {link.titulo}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
