8c792d13abb8375eb8841b2aaa588d1f1abc8374
astrosuka <yegresvoselok@gmail.com>
2025-02-19 21:08:35 -0300

    Refactor section error message and enhance section sorting in App component; update Sanity query to include 'quienSoy' type

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ function App() {
   const { data, isLoading, error } = useSections();
 
   if (isLoading) return <div>Cargando...</div>;
-  if (error) return <div>Hubo un error al cargar los programas</div>;
+  if (error) return <div>Hubo un error al cargar la data</div>;
+
+  const sectionOrder = ["visceral", "sonoro", "visual", "escrito", "quienSoy"];
 
   console.log(data);
   return (
     <>
       <motion.div drag className="animate-pulse cursor-grab">
         ☆*: .｡. o(≧▽≦)o .｡.:*☆pr1nc3s4-v4mp1r0(*/ω＼*)
-        {data.map((section) => (
-          <div key={section._id}>{section.titulo.es}</div>
-        ))}
+        {data
+          .sort((a, b) => {
+            return (
+              sectionOrder.indexOf(a._type) - sectionOrder.indexOf(b._type)
+            );
+          })
+          .map((section) => (
+            <div key={section._id}>{section.titulo.es}</div>
+          ))}
       </motion.div>
     </>
   );
--- src/sanity/sanity-utils.js
+++ src/sanity/sanity-utils.js
@@ -2,11 +2,12 @@ import { client } from "./client";
 
 export async function getSections() {
   return client.fetch(
-    `*[_type in ['visual', 'sonoro', 'escrito', 'visceral']] {
+    `*[_type in ['visual', 'sonoro', 'escrito', 'visceral', 'quienSoy']]{
             _id,
             slug,
             titulo,
             text,
+            _type
         }`,
   );
 }
