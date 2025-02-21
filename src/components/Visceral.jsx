97948f8fb601c6e7c4054e11e4c0f1debd61b1e7
sofffja <sofi.efron@gmail.com>
2025-02-21 01:07:41 +0000

    Refactor QuienSoy and Visceral components to use custom PortableText components and update rendering of data

--- src/components/Visceral.jsx
+++ src/components/Visceral.jsx
@@ -1,7 +1,7 @@
 import { useSections } from "../hooks/useData";
 import { PortableText } from "@portabletext/react";
 import useLanguage from "../hooks/useLanguage";
-import SectionTitle from "./SectionTitle";
+import { components } from "./PortableText/PortableTextComponents";
 
 export default function Visceral() {
   const { data, isLoading, error } = useSections();
@@ -12,23 +12,35 @@ export default function Visceral() {
 
   const visceralData = data.filter((i) => i._type === "visceral")[0];
 
+  console.log(visceralData);
+
   return (
-    <section>
-      <SectionTitle title={visceralData.titulo} />
+    <section className="pb-16">
+      <h1 className="almendra py-8 text-center text-8xl">
+        {visceralData.titulo}
+      </h1>
+
       {visceralData.descripcion && (
-        <PortableText
-          value={
-            visceralData.descripcion[language] || visceralData.descripcion.es
-          }
-        />
+        <div className="flex flex-col gap-2">
+          <PortableText
+            components={components}
+            value={
+              visceralData.descripcion[language] || visceralData.descripcion.es
+            }
+          />
+        </div>
       )}
+
       {visceralData.links && (
-        <ul>
-          {visceralData.links.map((link) => (
+        <ul className="flex flex-wrap justify-center gap-4 px-32 pt-16">
+          {visceralData.links.map((link, index) => (
             <li key={link._id}>
-              <a href={link.url} target="_blank">
+              <a className="hover:underline" href={link.url} target="_blank">
                 {link.titulo}
               </a>
+              {index !== visceralData.links.length - 1 && (
+                <span className="pl-4">⧂</span>
+              )}
             </li>
           ))}
         </ul>
