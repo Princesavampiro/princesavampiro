292a5deb5bd225993bc66d58ff3a46acc5882df9
astrosuka <yegresvoselok@gmail.com>
2025-02-25 14:45:52 -0300

    Refactor Release component to use ItemContainer, DraggableWindow, ImageGallery, and LinksList for improved layout and functionality

--- src/components/Release.jsx
+++ src/components/Release.jsx
@@ -1,14 +1,17 @@
 import { useRelease } from "../hooks/useData";
-import { useNavigate, useParams } from "react-router";
+import { NavLink, useParams } from "react-router";
 import Loading from "./Loading";
 import { PortableText } from "@portabletext/react";
 import useLanguage from "../hooks/useLanguage";
 import { components } from "./PortableText/PortableTextComponents";
+import ImageGallery from "./ImageGallery";
+import LinksList from "./LinksList";
+import DraggableWindow from "./DraggableWindow";
+import ItemContainer from "./ItemContainer";
 
 export default function Release() {
   const { slug } = useParams();
   const { data, isLoading, error } = useRelease(slug);
-  const navigate = useNavigate();
   const { language } = useLanguage();
 
   const size = {
@@ -21,9 +24,9 @@ export default function Release() {
       es: "Sello",
       en: "Label",
     },
-    goBack: {
-      es: "Volver",
-      en: "Go back",
+    close: {
+      es: "Cerrar",
+      en: "Close",
     },
   };
 
@@ -31,46 +34,29 @@ export default function Release() {
   if (error) return <div>Hubo un error :( </div>;
 
   return (
-    <section className="flex flex-col items-center justify-center gap-4">
-      <h1>{data[0].titulo}</h1>
-      <p>{data[0].fecha}</p>
-      {data[0].sello && <p>{fields.label[language] + ": " + data[0].sello}</p>}
-      {data[0].artwork && (
-        <img src={data[0].artwork.url + size.small} alt={data[0].titulo} />
-      )}
-      {data[0].texto && (
-        <PortableText
-          value={data[0].texto[language] || data[0].texto.es}
-          components={components}
-        />
-      )}
-
-      {/* embed */}
-
-      {data[0].imagenes && (
-        <ul className="flex flex-wrap gap-4">
-          {data[0].imagenes.map((img) => (
-            <li key={img._key} className="w-[200px]">
-              <img src={img.url + size.small} alt={data[0].titulo} />
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {data[0].links && (
-        <ul>
-          <h3 className="underline">Links:</h3>
-          {data[0].links.map((link) => (
-            <li key={link._id}>
-              <a href={link.url} target="_blank">
-                {link.titulo}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      <button onClick={() => navigate(-1)}>{fields.goBack[language]}</button>
-    </section>
+    <ItemContainer>
+      <NavLink to="..">{fields.close[language]}</NavLink>
+      <DraggableWindow>
+        <section className="flex flex-col items-center justify-center gap-4">
+          {data[0].titulo && <h1>{data[0].titulo}</h1>}
+          <p>{data[0].fecha}</p>
+          {data[0].sello && (
+            <p>{fields.label[language] + ": " + data[0].sello}</p>
+          )}
+          {data[0].artwork && (
+            <img src={data[0].artwork.url + size.small} alt={data[0].titulo} />
+          )}
+          {data[0].texto && (
+            <PortableText
+              value={data[0].texto[language] || data[0].texto.es}
+              components={components}
+            />
+          )}
+          {/* embed */}
+          {data[0].imagenes && <ImageGallery images={data[0].imagenes} />}
+          {data[0].links && <LinksList links={data[0].links} />}
+        </section>
+      </DraggableWindow>
+    </ItemContainer>
   );
 }
