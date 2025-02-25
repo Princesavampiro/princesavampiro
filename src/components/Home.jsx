8834b00cc2fcc9a90e1df2a0b5c06b4f07d77a8b
astrosuka <yegresvoselok@gmail.com>
2025-02-25 14:43:02 -0300

    Refactor Home component to include section sorting and navigation links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,38 @@
-export default function Home() {
+import { NavLink } from "react-router";
+import useLanguage from "../hooks/useLanguage";
+
+export default function Home({ sections }) {
+  const sectionOrder = ["visceral", "sonoro", "visual", "escrito", "quienSoy"];
+  const { language } = useLanguage();
+
+  if (!sections) return null;
   return (
-    <section className="fixed inset-0 -z-10 flex h-screen w-full animate-pulse flex-col items-center justify-center italic">
-      ✧
-      {/* <p>
-        estas en el bosque y te encuentras un caramelo con envoltorio a rayas
-        entre las hojas secas del piso
-      </p>
-      <p>~que haces?~</p>
-      <ul className="flex gap-2 pt-2">
-        <button className="cursor-pointer border px-2">
-          me lo como sin dudar
-        </button>
-        <button className="cursor-pointer border px-2">salgo corriendo</button>
-        <button className="cursor-pointer border px-2">sigo caminando</button>
-      </ul> */}
+    <section className="fixed inset-0 flex h-screen w-full animate-pulse items-center justify-center gap-4 italic">
+      <ul className="flex flex-wrap justify-center rounded-[50%] bg-[#000000ee] px-12 py-6 text-center font-[Nightingale] select-none">
+        {sections
+          .sort((a, b) => {
+            return (
+              sectionOrder.indexOf(a._type) - sectionOrder.indexOf(b._type)
+            );
+          })
+          .map((section, index) => (
+            <div key={section._id}>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "px-2 underline" : "px-2 hover:underline"
+                }
+                to={section.slug.current}
+              >
+                {section._type === "quienSoy"
+                  ? "???"
+                  : section.titulo[language] || section.titulo}
+              </NavLink>
+              {index !== sections.length - 1 && (
+                <span className="mx-auto">✴</span>
+              )}
+            </div>
+          ))}
+      </ul>
     </section>
   );
 }
