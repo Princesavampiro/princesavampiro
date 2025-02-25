ae13852e37ea350d6aad5ccb2e4d597c32fdaa26
astrosuka <yegresvoselok@gmail.com>
2025-02-25 14:50:30 -0300

    Refactor SectionInfo component to wrap content in DraggableWindow

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -1,15 +1,18 @@
 import { PortableText } from "@portabletext/react";
 import SectionTitle from "./SectionTitle";
 import { components } from "./PortableText/PortableTextComponents";
+import DraggableWindow from "./DraggableWindow";
 
 export default function SectionInfo({ title, text }) {
   return (
     <div className="flex flex-col gap-4">
-      <SectionTitle title={title} />
       {text && (
-        <div className="flex flex-col gap-4">
-          <PortableText components={components} value={text} />
-        </div>
+        <DraggableWindow>
+          <SectionTitle title={title} />
+          <div className="flex flex-col gap-4">
+            <PortableText components={components} value={text} />
+          </div>
+        </DraggableWindow>
       )}
     </div>
   );
