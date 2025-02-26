6eceb544200e7c1e55951d08c6efb0106cbc7007
sofffja <sofi.efron@gmail.com>
2025-02-26 16:53:48 +0000

    Add custom hook to manage current section based on URL path

---
+++ src/hooks/useCurrentSection.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { useSections } from "../hooks/useData";
+import { useLocation } from "react-router";
+
+export function useCurrentSection() {
+  const { data, isLoading, error } = useSections();
+  const [currentSection, setCurrentSection] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (data) {
+      const pathname = location.pathname.substring(1);
+      const foundSection = data.find(
+        (section) => section.slug.current === pathname,
+      );
+      setCurrentSection(foundSection || null);
+    }
+  }, [location.pathname, data]);
+
+  return { currentSection, isLoading, error };
+}
