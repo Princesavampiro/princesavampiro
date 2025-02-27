9ceb8f05063d61844723c7b2c458311c47fb037e
astrosuka <yegresvoselok@gmail.com>
2025-02-27 12:09:26 -0300

    Implement data fetching and loading state in ItemContainer component

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,7 +1,19 @@
-export default function ItemContainer({ children }) {
+import { NavLink, useLocation } from "react-router";
+import { useItem } from "../hooks/useData";
+import Loading from "./Loading";
+
+export default function ItemContainer() {
+  const location = useLocation();
+  const currentPath = location.pathname.split("/")[2];
+  const { data, isLoading, error } = useItem(currentPath);
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>Hubo un error :(</div>;
+
   return (
-    <section className="flex h-screen w-screen flex-col items-center justify-center gap-4 bg-[#0000022] backdrop-blur-sm">
-      <div className="h-3/4">{children}</div>
+    <section className="fixed flex h-screen w-screen flex-col items-center justify-center gap-4 bg-[#0000022] backdrop-blur-sm">
+      <h1>{data[0].titulo}</h1>
+      <NavLink to={location.pathname.split("/")[1]}>CERRAR</NavLink>
     </section>
   );
 }
