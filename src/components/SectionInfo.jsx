33646c40a71ff29eb7c578deecdc8c1a765fb4f1
sofffja <sofi.efron@gmail.com>
2025-02-27 19:57:06 +0000

    Add details prop to SectionInfo component for additional information display

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -2,17 +2,30 @@ import { PortableText } from "@portabletext/react";
 import { components } from "./PortableText/PortableTextComponents";
 import DraggableWindow from "./DraggableWindow";
 
-export default function SectionInfo({ windowTitle, sectionTitle, text }) {
+export default function SectionInfo({
+  windowTitle,
+  sectionTitle,
+  details,
+  text,
+}) {
   if (!windowTitle && !sectionTitle && !text) return null;
 
   return (
     <DraggableWindow title={windowTitle} className={"max-h-[40vh] max-w-prose"}>
       <div className="p-4">
         {sectionTitle && (
-          <h1 className="py-8 pl-8 font-[Nightingale] text-2xl">
-            {sectionTitle}
-          </h1>
+          <h1 className="py-4 font-[Nightingale] text-2xl">{sectionTitle}</h1>
         )}
+
+        {details && (
+          <div className="pb-4 text-sm">
+            {details.date && <p>{details.date}</p>}
+            {details.label && <p>{details.label}</p>}
+            {details.releaseType && <p>{details.releaseType}</p>}
+            {details.place && <p>{details.place}</p>}
+          </div>
+        )}
+
         {text && (
           <div className="flex flex-col gap-4">
             <PortableText components={components} value={text} />
