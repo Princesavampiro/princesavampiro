bae5e9437583b088c1ed0826f3db5201bc0ef256
astrosuka <yegresvoselok@gmail.com>
2025-03-04 15:19:49 -0300

    Update style

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,7 +1,9 @@
 export default function LoadingScreen() {
   return (
-    <div className="flex h-screen animate-pulse items-center justify-center">
-      Cargando...
+    <div className="fixed inset-0 grid place-items-center">
+      <div className="m-8 flex items-center justify-center">
+        <div className="size-4 animate-ping rounded-full border" />
+      </div>
     </div>
   );
 }
