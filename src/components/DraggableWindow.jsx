bfd555f8fab3a4469e66b1fe9e51760e486d5ac0
astrosuka <yegresvoselok@gmail.com>
2025-03-05 17:21:41 -0300

    Add z-index on hover and active to improve user experience

--- src/components/DraggableWindow.jsx
+++ src/components/DraggableWindow.jsx
@@ -12,7 +12,7 @@ export default function DraggableWindow({ title, children, className }) {
       dragControls={controls}
       whileDrag={{ scale: 1.05, cursor: "grabbing" }}
       dragMomentum={false}
-      className={`flex h-full flex-col overflow-hidden rounded-lg border-white/30 bg-[#00000022] drop-shadow-[0_0_12px_#000] backdrop-blur-xl select-none sm:border sm:backdrop-blur-sm ${className}`}
+      className={`flex h-full flex-col overflow-hidden rounded-lg border-white/30 bg-[#00000022] drop-shadow-[0_0_12px_#000] backdrop-blur-xl select-none sm:border sm:backdrop-blur-sm ${className} hover:z-30 active:z-30`}
     >
       {!isMobile && (
         <div
