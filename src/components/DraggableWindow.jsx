b2b8c4dd4b4fe031cee9a4c8bea56fb08505d117
astrosuka <yegresvoselok@gmail.com>
2025-03-05 06:37:11 -0300

    Remove drag and adjust style for mobile

--- src/components/DraggableWindow.jsx
+++ src/components/DraggableWindow.jsx
@@ -1,7 +1,9 @@
 import { motion, useDragControls } from "motion/react";
+import useIsMobile from "../hooks/useIsMobile";
 
 export default function DraggableWindow({ title, children, className }) {
   const controls = useDragControls();
+  const isMobile = useIsMobile();
 
   return (
     <motion.div
@@ -10,14 +12,16 @@ export default function DraggableWindow({ title, children, className }) {
       dragControls={controls}
       whileDrag={{ scale: 1.05, cursor: "grabbing" }}
       dragMomentum={false}
-      className={`flex h-full flex-col overflow-hidden rounded-lg border border-white/30 bg-[#00000022] drop-shadow-[0_0_12px_#000] backdrop-blur-sm select-none ${className}`}
+      className={`flex h-full flex-col overflow-hidden rounded-lg border-white/30 bg-[#00000022] drop-shadow-[0_0_12px_#000] backdrop-blur-xl select-none sm:border sm:backdrop-blur-sm ${className}`}
     >
-      <div
-        onPointerDown={(event) => controls.start(event)}
-        className="flex min-h-8 cursor-grab items-center justify-center border-b active:cursor-grabbing"
-      >
-        {title ?? "✴"}
-      </div>
+      {!isMobile && (
+        <div
+          onPointerDown={(event) => controls.start(event)}
+          className="flex min-h-8 cursor-grab items-center justify-center border-b active:cursor-grabbing"
+        >
+          {title ?? "✴"}
+        </div>
+      )}
       <div className="overflow-y-auto">{children}</div>
     </motion.div>
   );
