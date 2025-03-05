9e8bc2f0c53be8eda1474fcf90b99b5fc76ca6e4
sofffja <sofi.efron@gmail.com>
2025-03-05 21:00:30 +0000

    Update Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="m-8 flex items-center justify-center">
+    <div className="user-select-none pointer-events-none m-8 flex h-screen w-full items-center justify-center">
       <div className="size-4 animate-ping rounded-full border" />
     </div>
   );
