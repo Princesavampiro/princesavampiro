1eca2701e4c9df7986364f4d1141ee24534876fb
astrosuka <yegresvoselok@gmail.com>
2025-03-05 02:50:49 -0300

    Change circle to star

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 grid place-items-center">
       <div className="m-8 flex items-center justify-center">
-        <div className="size-4 animate-ping rounded-full border" />
+        <div className="animate-spin text-2xl">✴</div>
       </div>
     </div>
   );
