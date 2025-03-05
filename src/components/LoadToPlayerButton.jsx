eac085f8ad311fd183c75fe1ef13369b557a670f
astrosuka <yegresvoselok@gmail.com>
2025-03-05 00:05:03 -0300

    Create button to load embeds to Player

---
+++ src/components/LoadToPlayerButton.jsx
@@ -0,0 +1,19 @@
+import usePlayer from "../hooks/usePlayer";
+import DraggableWindow from "./DraggableWindow";
+
+export default function LoadToPlayerButton({ data, className }) {
+  const { currentEmbed, setCurrentEmbed } = usePlayer();
+
+  if (currentEmbed === data) return null;
+
+  return (
+    <DraggableWindow className={className}>
+      <button
+        className="cursor-pointer bg-yellow-300 px-4 py-2 font-[Nightingale] text-6xl text-red-500 uppercase"
+        onClick={() => setCurrentEmbed(data)}
+      >
+        play
+      </button>
+    </DraggableWindow>
+  );
+}
