2ebb79dae5dadc6c26c711c114182b4a5d3c2855
astrosuka <yegresvoselok@gmail.com>
2025-03-04 23:57:51 -0300

    Add PlayerProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LanguageProvider } from "./context/LanguageContext.jsx";
+import { PlayerProvider } from "./context/PlayerContext.jsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +21,9 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <LanguageProvider>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <PlayerProvider>
+            <App />
+          </PlayerProvider>
         </QueryClientProvider>
       </LanguageProvider>
     </BrowserRouter>
