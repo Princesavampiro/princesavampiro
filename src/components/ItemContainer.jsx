29a250795fd0b26a98d64d3922b96c4a003545f6
astrosuka <yegresvoselok@gmail.com>
2025-03-06 14:01:22 -0300

    Increaze z on hover and active

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -124,7 +124,7 @@ export default function ItemContainer() {
       {data[0].embed && (
         <LoadToPlayerButton
           data={data[0].embed}
-          className="max-w-content fixed bottom-1/8 left-1/4 h-max"
+          className="max-w-content fixed bottom-1/8 left-1/4 h-max hover:z-50 active:z-50"
         />
       )}
     </section>
