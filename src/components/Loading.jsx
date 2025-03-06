a10a7a7496bed6b88b1d197553dc5c9fb28befe3
astrosuka <yegresvoselok@gmail.com>
2025-03-06 16:50:45 -0300

    Make loading animation more consisten across sections

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="user-select-none pointer-events-none m-8 flex h-screen w-full items-center justify-center">
-      <div className="size-4 animate-ping rounded-full border" />
+    <div className="user-select-none pointer-events-none m-8 flex items-center justify-center">
+      <div className="animate-spin text-2xl">✴</div>
     </div>
   );
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,9 +1,9 @@
+import Loading from "./Loading";
+
 export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 grid place-items-center">
-      <div className="m-8 flex items-center justify-center">
-        <div className="animate-spin text-2xl">✴</div>
-      </div>
+      <Loading />
     </div>
   );
 }
