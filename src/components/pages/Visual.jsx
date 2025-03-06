5585c2aa6d121ca11b240b9f4b7245e435918fad
astrosuka <yegresvoselok@gmail.com>
2025-03-06 16:16:41 -0300

    Clean up code

--- src/components/pages/Visual.jsx
+++ src/components/pages/Visual.jsx
@@ -1,25 +1,17 @@
 import { useExpos, useSections } from "../../hooks/useData";
-import useLanguage from "../../hooks/useLanguage";
 import CardGrid from "../CardGrid";
 import Error from "../Error";
 
-import Loading from "../Loading";
-import SectionInfo from "../SectionInfo";
-
 export default function Visual() {
-  const { data, isLoading, error } = useSections();
+  const { isLoading, error } = useSections();
   const {
     data: expos,
     isLoading: isExposLoading,
     error: exposError,
   } = useExpos();
 
-  const { language } = useLanguage();
-
   if (isLoading || isExposLoading || !expos) return;
   if (error || exposError) return <Error />;
-
-  const visualData = data.filter((i) => i._type === "visual")[0];
 
   return (
     <div className="p-8">
