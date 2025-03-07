76bd7856978bd02771fd1a15fb371780742363da
astrosuka <yegresvoselok@gmail.com>
2025-03-07 16:53:05 -0300

    Increase z-index for LanguageButton and ThemeToggle for visibility while ItemContainer is displayed

--- src/components/LanguageButton.jsx
+++ src/components/LanguageButton.jsx
@@ -5,7 +5,7 @@ export default function LanguageButton() {
   return (
     <div
       onClick={() => setLanguage(language === "es" ? "en" : "es")}
-      className="fixed top-8 right-4 flex cursor-pointer items-center justify-center rounded-full border border-white/30 bg-black/10 p-3 text-xs backdrop-blur-md select-none hover:bg-white/30 hover:brightness-120 sm:right-8"
+      className="fixed top-8 right-4 z-70 flex cursor-pointer items-center justify-center rounded-full border border-white/30 bg-black/10 p-3 text-xs backdrop-blur-md select-none hover:bg-white/30 hover:brightness-120 sm:right-8"
     >
       {language == "es" ? "EN" : "ES"}
     </div>
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,7 +6,7 @@ export default function ThemeToggle({ themes, currentTheme, setCurrentTheme }) {
           themes[(themes.indexOf(currentTheme) + 1) % themes.length],
         )
       }
-      className="fixed top-20 right-4 z-50 flex size-5 cursor-pointer items-center justify-center rounded-full border border-white/30 text-xs backdrop-blur-md select-none hover:animate-spin hover:border-0 hover:bg-white/30 hover:bg-conic/decreasing hover:from-violet-700 hover:via-lime-300 hover:to-violet-700 hover:brightness-120 sm:right-4"
+      className="fixed top-20 right-4 z-70 flex size-5 cursor-pointer items-center justify-center rounded-full border border-white/30 text-xs backdrop-blur-md select-none hover:animate-spin hover:border-0 hover:bg-white/30 hover:bg-conic/decreasing hover:from-violet-700 hover:via-lime-300 hover:to-violet-700 hover:brightness-120 sm:right-4"
     />
   );
 }
