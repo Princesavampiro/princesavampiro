942e61f2de0dd7bfb4abdb2cebe2de75ba442b14
astrosuka <yegresvoselok@gmail.com>
2025-03-06 23:18:23 -0300

    Refactor ActionBar to use useLocation for navigation and simplify back button logic

--- src/components/ActionBar.jsx
+++ src/components/ActionBar.jsx
@@ -1,26 +1,16 @@
-import { NavLink, useNavigate } from "react-router";
+import { NavLink, useLocation } from "react-router";
 
 export default function ActionBar() {
-  const navigate = useNavigate();
-  const historyIndex = window.history.state?.idx;
-
-  const handleBack = () => {
-    if (historyIndex > 0) {
-      navigate(-1);
-    }
-  };
+  const location = useLocation();
 
   return (
     <div className="fixed bottom-4 left-1/2 z-100 flex -translate-x-1/2 items-center justify-center gap-4 rounded-full border border-white/30 bg-black/30 p-3 text-4xl backdrop-blur-md sm:bg-black/5">
       <NavLink
-        onClick={handleBack}
-        aria-label="Back"
-        className={`flex size-12 items-center justify-center rounded-full pt-1 text-center font-[Crozette] leading-none ${!historyIndex || historyIndex === 0 ? "cursor-default opacity-40" : "cursor-pointer hover:bg-white/20"}`}
-      >
-        {`<--`}
-      </NavLink>
-      <NavLink
-        to="/"
+        to={
+          location.pathname.split("/").length > 2
+            ? "../" + location.pathname.split("/")[1]
+            : "/"
+        }
         aria-label="Home"
         className="flex size-12 cursor-pointer items-center justify-center rounded-full pt-1 pl-0.5 text-center font-[Crozette] leading-none hover:bg-white/20"
       >
