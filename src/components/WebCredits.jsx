fe53726e1b8f96583b6a136d828cebea264d092c
astrosuka <yegresvoselok@gmail.com>
2025-09-15 23:24:06 -0300

    Update web credits

--- src/components/WebCredits.jsx
+++ src/components/WebCredits.jsx
@@ -4,18 +4,14 @@ export default function WebCredits() {
   const { language } = useLanguage();
   return (
     <div className="right-8 bottom-8 flex w-full justify-center gap-2 rounded-full border-white/30 py-4 text-sm sm:fixed sm:w-auto sm:border sm:px-4 sm:py-2 sm:backdrop-blur-lg">
-      <div>
-        {language === "es"
-          ? "diseño y desarrollo web:"
-          : "web design and development:"}
-      </div>
+      <div>web: </div>
       <div>
         <a
-          href="https://i10-studio.github.io"
+          href="https://i10.dev"
           target="_blank"
           className="animate-pulse hover:animate-none hover:underline"
         >
-          instrumento
+          i10.dev
         </a>
       </div>
     </div>
